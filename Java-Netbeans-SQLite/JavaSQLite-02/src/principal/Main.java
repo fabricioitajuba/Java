ca0c48d987efdc287facb0b1b43febcfc08b9061
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-04-13 11:57:18 -0300

    Atualização

--- Java-Netbeans-SQLite/JavaSQLite-02/src/principal/Main.java
+++ Java-Netbeans-SQLite/JavaSQLite-02/src/principal/Main.java
@@ -24,57 +24,68 @@ public class Main {
         boolean teste;
         BancoSQLite bancoSQLite = new BancoSQLite();
         
+        //Cria banco de dados
         teste = bancoSQLite.criarBanco("banco.db");
         System.out.println(teste);
         
+        //Cria tabela no banco de dados
         teste = bancoSQLite.criarTabela("banco.db", "tbl_pessoa");
         System.out.println(teste);
         
+        //Insere registro
         bancoSQLite.setId(1);
         bancoSQLite.setNome("Fabricio");
         bancoSQLite.setIdade(40);
         teste = bancoSQLite.insert("banco.db", "tbl_pessoa");
         System.out.println(teste);
 
+        //Insere registro
         bancoSQLite.setId(2);
         bancoSQLite.setNome("Angiene");
         bancoSQLite.setIdade(47);        
         teste = bancoSQLite.insert("banco.db", "tbl_pessoa");   
         System.out.println(teste);        
         
+        //Insere registro
         bancoSQLite.setId(3);
         bancoSQLite.setNome("Caio");
         bancoSQLite.setIdade(5);        
         teste = bancoSQLite.insert("banco.db", "tbl_pessoa");        
         System.out.println(teste);
 
+        //Insere registro
         bancoSQLite.setId(4);
         bancoSQLite.setNome("Fabiano");
         bancoSQLite.setIdade(43);        
         teste = bancoSQLite.insert("banco.db", "tbl_pessoa");        
         System.out.println(teste);        
         
+        //Insere registro
         bancoSQLite.setId(5);
         bancoSQLite.setNome("Fiica");
         bancoSQLite.setIdade(75);        
         teste = bancoSQLite.insert("banco.db", "tbl_pessoa");        
         System.out.println(teste);         
 
+        //Altera registro
         bancoSQLite.setId(1);
         bancoSQLite.setNome("Fabricio");
         bancoSQLite.setIdade(44);        
         teste = bancoSQLite.update("banco.db", "tbl_pessoa");
         System.out.println(teste);
         
+        //Deleta registro
         bancoSQLite.setId(4);
         teste = bancoSQLite.delete("banco.db", "tbl_pessoa");        
         System.out.println(teste);    
         
+        ////Busca registro
         bancoSQLite.setId(5);
         teste = bancoSQLite.buscaDado("banco.db", "tbl_pessoa");
         System.out.println("id=" + bancoSQLite.getId() + " nome=" + bancoSQLite.getNome() + " idade=" + bancoSQLite.getIdade());
 
-        ArrayList<TabelaDado> listaDePessoas = new ArrayList<>();
+        //Busca registros
+        ArrayList<TabelaDado> listaDePessoas = new ArrayList<>();               //#
         
         for(TabelaDado p: bancoSQLite.buscaDados("banco.db", "tbl_pessoa")){
 
@@ -87,10 +98,12 @@ public class Main {
             listaDePessoas.add(p);
         }        
         
-        System.out.println("\n"); 
-        for(int i=0; i<listaDePessoas.size(); i++){
-            System.out.println("Nome: " + listaDePessoas.get(i).getNome());   
-            System.out.println("Idade: " + listaDePessoas.get(i).getIdade()); 
+        System.out.println("\n");                                               //#
+        for(int i=0; i<listaDePessoas.size(); i++){                             //#
+            System.out.println("Id: " + listaDePessoas.get(i).getId());         //#   
+            System.out.println("Nome: " + listaDePessoas.get(i).getNome());     //#   
+            System.out.println("Idade: " + listaDePessoas.get(i).getIdade());   //# 
+            System.out.println("\n");                                           //#
         }        
 
     }
