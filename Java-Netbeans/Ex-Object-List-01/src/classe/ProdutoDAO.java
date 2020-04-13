2df427c86631ef1dd3c5ec931d15209352f7e711
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-04-13 10:32:39 -0300

    Atualização

--- Java-Netbeans/Ex-Object-List-01/src/classe/ProdutoDAO.java
+++ Java-Netbeans/Ex-Object-List-01/src/classe/ProdutoDAO.java
@@ -18,25 +18,48 @@ public class ProdutoDAO {
 
         List<Produto> produtos = new ArrayList<>();
 
-        Produto produto = new Produto();        
+//        Produto produto = new Produto();      
+//        
+//        produto.setId(1);
+//        produto.setDescricao("Teste1");
+//        produto.setQtd(10);
+//        produto.setPreco(10.2);
+//        produtos.add(produto);
+//           
+//        produto.setId(2);
+//        produto.setDescricao("Teste2");
+//        produto.setQtd(20);
+//        produto.setPreco(20.2);
+//        produtos.add(produto);    
+//        
+//        produto.setId(3);
+//        produto.setDescricao("Teste3");
+//        produto.setQtd(30);
+//        produto.setPreco(30.2);
+//        produtos.add(produto);        
+
+
+        Produto produto1 = new Produto();   
+        Produto produto2 = new Produto();
+        Produto produto3 = new Produto();
         
-        produto.setId(1);
-        produto.setDescricao("Teste1");
-        produto.setQtd(10);
-        produto.setPreco(10.2);
-        produtos.add(produto);
-          
-        produto.setId(2);
-        produto.setDescricao("Teste2");
-        produto.setQtd(20);
-        produto.setPreco(20.2);
-        produtos.add(produto);    
-         
-        produto.setId(3);
-        produto.setDescricao("Teste3");
-        produto.setQtd(30);
-        produto.setPreco(30.2);
-        produtos.add(produto);        
+        produto1.setId(1);
+        produto1.setDescricao("Teste1");
+        produto1.setQtd(10);
+        produto1.setPreco(10.2);
+        produtos.add(produto1);
+           
+        produto2.setId(2);
+        produto2.setDescricao("Teste2");
+        produto2.setQtd(20);
+        produto2.setPreco(20.2);
+        produtos.add(produto2);    
+        
+        produto3.setId(3);
+        produto3.setDescricao("Teste3");
+        produto3.setQtd(30);
+        produto3.setPreco(30.2);
+        produtos.add(produto3); 
 
         return produtos;
 
