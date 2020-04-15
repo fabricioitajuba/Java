958a87b947cb00609983bc6871487c9c41bd62e1
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-04-15 18:50:11 -0300

    Atualização

---
+++ Java-Netbeans-Swing/ExJTable01/src/classe/Valores.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classe;
+
+/**
+ *
+ * @author fabricio
+ */
+public class Valores {
+ 
+    private int Valor1, Valor2, Valor3, Valor4;
+
+    public int getValor1() {
+        return Valor1;
+    }
+
+    public void setValor1(int Valor1) {
+        this.Valor1 = Valor1;
+    }
+
+    public int getValor2() {
+        return Valor2;
+    }
+
+    public void setValor2(int Valor2) {
+        this.Valor2 = Valor2;
+    }
+
+    public int getValor3() {
+        return Valor3;
+    }
+
+    public void setValor3(int Valor3) {
+        this.Valor3 = Valor3;
+    }
+
+    public int getValor4() {
+        return Valor4;
+    }
+
+    public void setValor4(int Valor4) {
+        this.Valor4 = Valor4;
+    }
+    
+}
