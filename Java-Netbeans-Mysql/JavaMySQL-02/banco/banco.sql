09af3560fcaf5603161aed360ab8db429bedd9ce
Fabricio de Lima Ribeiro <fabricio.engenheiro@gmail.com>
2020-04-18 10:32:19 -0300

    Atualização

---
+++ Java-Netbeans-Mysql/JavaMySQL-02/banco/banco.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: 18-Abr-2020 às 10:25
+-- Versão do servidor: 10.1.44-MariaDB-0+deb9u1
+-- PHP Version: 7.0.33-0+deb9u7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoas`
+--
+
+CREATE TABLE `pessoas` (
+  `id` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `idade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `pessoas`
+--
+
+INSERT INTO `pessoas` (`id`, `nome`, `idade`) VALUES
+(1, 'Fabricio', 44),
+(2, 'Caio', 5),
+(3, 'Angiene', 47);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `senhas`
+--
+
+CREATE TABLE `senhas` (
+  `id` int(11) NOT NULL,
+  `login` text NOT NULL,
+  `senha` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `senhas`
+--
+
+INSERT INTO `senhas` (`id`, `login`, `senha`) VALUES
+(1, 'fabricio', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pessoas`
+--
+ALTER TABLE `pessoas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `senhas`
+--
+ALTER TABLE `senhas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`(30));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pessoas`
+--
+ALTER TABLE `pessoas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `senhas`
+--
+ALTER TABLE `senhas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Java-Netbeans-Mysql/JavaMySQL-02/src/classes/BancoMySQL.java
@@ -0,0 +1,311 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author fabricio
+ */
+public class BancoMySQL {
+    
+    private int Id, Idade;
+    private String Nome;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public int getIdade() {
+        return Idade;
+    }
+
+    public void setIdade(int Idade) {
+        this.Idade = Idade;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://192.168.0.2:3306/banco";
+    private static final String USER = "admin";
+    private static final String PASS = "";
+    
+    public static Connection getConnection(){
+    
+        try {
+            Class.forName(DRIVER);            
+            return (Connection) DriverManager.getConnection(URL, USER, PASS);            
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro na conexao: ", ex);
+        }        
+    }
+    
+    public static void closeConnection(Connection con){
+            
+        try {
+            if(con != null){
+                con.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro na conexao: " + ex);
+        }                
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt){
+
+        closeConnection(con);
+        
+        try {
+            
+            if(stmt != null){
+                stmt.close();
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Erro na conexao: " + ex);
+        }                
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs){
+
+        closeConnection(con, stmt);
+        
+        try {
+            
+            if(rs != null){
+                rs.close();
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Erro na conexao: " + ex);
+        }                
+    }    
+    
+    //Inserir dados
+    public boolean Insert(String Tabela){
+    
+        Connection con = getConnection();
+        PreparedStatement stmt = null;
+        boolean ok = false;
+        
+        try {
+            stmt = con.prepareStatement("INSERT INTO "+Tabela+" (id, nome, idade) VALUES(?, ?, ?)");
+            stmt.setInt(1, Id);
+            stmt.setString(2, Nome);
+            stmt.setInt(3, Idade);
+            
+            stmt.executeUpdate();       
+            
+            //JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+            ok = true;
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex);
+        } finally{
+            closeConnection(con, stmt);
+        }       
+        return ok;
+    }
+
+    //Deletar dado
+    public boolean Delete(String Tabela){
+    
+        Connection con = getConnection();
+        PreparedStatement stmt = null;
+        boolean ok = false;
+        
+        try {
+            stmt = con.prepareStatement("DELETE FROM "+Tabela+" WHERE id = ?");
+            stmt.setInt(1, Id);            
+            stmt.executeUpdate();       
+            
+            //Organizar Id
+//            stmt = con.prepareStatement("SET @count = 0");            
+//            stmt.executeUpdate();
+//            stmt = con.prepareStatement("UPDATE `produto` SET `produto`.`id` = @count:= @count + 1");            
+//            stmt.executeUpdate();
+//            stmt = con.prepareStatement("ALTER TABLE `dbmercadinho`.`produto` AUTO_INCREMENT = 1");            
+//            stmt.executeUpdate();            
+            
+            //JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+            ok = true;
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex);
+        } finally{
+            closeConnection(con, stmt);
+        }     
+        return ok;
+    }
+ 
+    //Atualizar dados
+    public boolean update(String Tabela){
+    
+        Connection con = getConnection();
+        PreparedStatement stmt = null;
+        boolean ok = false;
+        
+        try {
+            stmt = con.prepareStatement("UPDATE "+Tabela+" SET nome = ?, idade = ? WHERE id = ?");
+            stmt.setString(1, Nome);
+            stmt.setInt(2, Idade);
+            stmt.setInt(3, Id);
+            
+            stmt.executeUpdate();       
+            
+            //JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
+            ok = true;
+            
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex);
+        } finally{
+            closeConnection(con, stmt);
+        }     
+        return ok;
+    }      
+
+    //Ler dado
+    public boolean buscaDado(String Tabela){
+
+        Connection con = getConnection();
+        PreparedStatement stmt = null;        
+        ResultSet rs = null;
+        boolean ok = false;
+        
+        try {            
+            stmt = con.prepareStatement("SELECT * FROM "+Tabela+" WHERE id = ?;");
+            stmt.setInt(1, Id);
+            rs = stmt.executeQuery();
+            
+            if(rs.next()){                               
+                Id = rs.getInt("id");
+                Nome = rs.getString("nome");
+                Idade = rs.getInt("idade");
+            }
+            ok = true;
+                    
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex);
+        } finally{
+            closeConnection(con, stmt, rs);
+        }        
+        return ok;
+    }
+    
+    //Ler dados
+    public List<BancoMySQL> buscaDados(String Tabela){
+
+        Connection con = getConnection();
+        PreparedStatement stmt = null;        
+        ResultSet rs = null;
+        
+        List<BancoMySQL> tabelas = new ArrayList<>();
+        
+        try {
+            
+            stmt = con.prepareStatement("SELECT * FROM "+Tabela);
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                
+                BancoMySQL bancoMySQL = new BancoMySQL();    
+                
+                bancoMySQL.setId(rs.getInt("id"));
+                bancoMySQL.setNome(rs.getString("nome"));
+                bancoMySQL.setIdade(rs.getInt("idade"));
+
+                tabelas.add(bancoMySQL);
+            }
+                    
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex);
+        } finally{
+            closeConnection(con, stmt, rs);
+        }
+        
+        return tabelas;
+    }
+
+    //Busca registros e salva em uma lista
+    public List<BancoMySQL> buscaNomes(String Tabela){
+
+        Connection con = getConnection();
+        PreparedStatement stmt = null;        
+        ResultSet rs = null;
+        
+        List<BancoMySQL> tabelas = new ArrayList<>();
+        
+        try {
+            
+            stmt = con.prepareStatement("SELECT * FROM "+Tabela+" WHERE nome LIKE ?;");
+            stmt.setString(1, "%"+Nome+"%");
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                
+                BancoMySQL bancoMySQL = new BancoMySQL();    
+                
+                bancoMySQL.setId(rs.getInt("id"));
+                bancoMySQL.setNome(rs.getString("nome"));
+                bancoMySQL.setIdade(rs.getInt("idade"));
+
+                tabelas.add(bancoMySQL);
+            }
+                    
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex);
+        } finally{
+            closeConnection(con, stmt, rs);
+        }
+        
+        return tabelas;
+    }    
+    
+    //Faz login
+    public boolean fazerLogin(String Tabela, String Login, String Senha){
+
+        Connection con = getConnection();
+        PreparedStatement stmt = null;        
+        ResultSet rs = null;
+        boolean ok = false;
+        
+        try {            
+            stmt = con.prepareStatement("SELECT * FROM "+Tabela+" WHERE login = ? and senha = ?;");
+            stmt.setString(1, Login);
+            stmt.setString(2, Senha);
+            rs = stmt.executeQuery();
+            
+            if(rs.next()){                               
+                ok=true;
+            }
+                    
+        } catch (SQLException ex) {
+            System.out.println("Erro " + ex);
+        } finally{
+            closeConnection(con, stmt, rs);
+        }        
+        return ok;
+    }    
+    
+}
